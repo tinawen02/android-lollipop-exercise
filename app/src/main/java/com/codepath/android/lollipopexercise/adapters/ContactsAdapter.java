4daf9f2cab9fe99b357c7716cf9577b5ef4a4d22
Tina Wen <tinawen@fb.com>
2022-06-08 11:41:59 -0700

    finished step 4

--- app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
@@ -2,6 +2,7 @@ package com.codepath.android.lollipopexercise.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.activities.DetailsActivity;
 import com.codepath.android.lollipopexercise.models.Contact;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -43,6 +47,7 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
         holder.rootView.setTag(contact);
         holder.tvName.setText(contact.getName());
         Glide.with(mContext).load(contact.getThumbnailDrawable()).centerCrop().into(holder.ivProfile);
+
     }
 
     @Override
@@ -51,7 +56,7 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
     }
 
     // Provide a reference to the views for each contact item
-    public class VH extends RecyclerView.ViewHolder {
+    public class VH extends RecyclerView.ViewHolder implements View.OnClickListener {
         final View rootView;
         final ImageView ivProfile;
         final TextView tvName;
@@ -65,16 +70,21 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
             vPalette = itemView.findViewById(R.id.vPalette);
 
             // Navigate to contact details activity on click of card view.
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    final Contact contact = (Contact)v.getTag();
-                    if (contact != null) {
-                        // Fire an intent when a contact is selected
-                        // Pass contact object in the bundle and populate details activity.
-                    }
-                }
-            });
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                Contact contact = mContacts.get(position);
+                Intent intent = new Intent(mContext, DetailsActivity.class);
+                intent.putExtra(DetailsActivity.EXTRA_CONTACT, contact);
+                // show the activity
+                mContext.startActivity(intent);
+            }
         }
     }
 }
